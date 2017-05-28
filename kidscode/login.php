e4ee64e38799cf40f5154f28205b4c5deaea9c7a
Solomon Negash <solomon@freethechildren.com>
2017-05-27 23:37:56 -0400

    adding login.php and under_construction.jpg to kidscode folder

---
+++ kidscode/login.php
@@ -0,0 +1,1 @@
+<div style="background: url('under_construction.jpg') center; background-size: cover;padding: 25% 20px;color: #FFF;text-align: center;font-size: 80px;font-weight: 700;">UNDER CONSTRUCTION</div>
